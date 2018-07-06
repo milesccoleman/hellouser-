1a0555bfc0f1c4d81065eb4cad4153140cc29e54
automatedcommunication <aletheia@aletheias-MacBook-Air.local>
2018-07-06 09:16:14 -0700

    fixed formatting

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,23 @@
 <template>
-<body>
-      <h1>{{ msg }}</h1>
-</body>
+  <body>
+    <h1>{{ msg }}</h1>
+  </body>
 </template>
 
 <script>
-var msg = prompt("What is your name?");
-  document.write ("Oh my gosh, " + msg + ", we've just made a unique web experience together!");
-
-
-
+var msg = prompt('What is your name?')
+document.write('Oh my gosh, ' + msg + ', we just made a unique web experience together!')
 </script>
 
 <style>
-
 body {
-	background-color: #f0db4f;
-	font-family: sans-serif;
-	text-align: center;
-	color: #323330;
-	font-size:  100px;
+background-color: #f0db4f;
+font-family: sans-serif;
+text-align: center;
+color: #323330;
+font-size:  100px;
 }
-
 h1 {
-	margin-top: -150px;
-
+margin-top: -150px;
 }
 </style>
