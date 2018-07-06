6d1ed88f4b811ebe5a7c0ea2b2ca6e52db71af06
automatedcommunication <aletheia@aletheias-air.seattleu.edu>
2018-07-05 19:48:01 -0700

    revised html, and added styles

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+<body>
+      <h1>{{ msg }}</h1>
+</body>
+</template>
+
+<script>
+var msg = prompt("What is your name?");
+  document.write ("Oh, my gosh, " + msg + ", we've just made a unique web experience together!");
+
+
+
+</script>
+
+<style>
+
+body {
+	background-color: #f0db4f;
+	font-family: sans-serif;
+	text-align: center;
+	color: #323330;
+	font-size:  100px;
+}
+
+h1 {
+	margin-top: -150px;
+
+}
+</style>
